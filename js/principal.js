5d976b8b5d924548f8083141ee899a63da21ab39
Héctor O'Higgins <guille_081195@outloook.com>
2024-10-14 16:31:19 -0500

    finalizado

--- js/principal.js
+++ js/principal.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function(){
 
     // recuperar nombre del usuario del localStorage
     const result = JSON.parse(this.localStorage.getItem('result'));
-    mostrarAlerta(result.nombreUsuario);
+    mostrarAlerta(`Bienvenido ${result.nombreUsuario}`);
 
 });
 
@@ -17,4 +17,29 @@ function mostrarAlerta(mensaje) {
 function ocultarAlerta() {
     msgSuccess.innerHTML = '';
     msgSuccess.style.display = 'none';
+}
+
+function cerrarSesion() {
+    fetch('http://localhost:8083/login/logout-async', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            tipoDocumento: JSON.parse(localStorage.getItem('result')).tdoc,
+            numeroDocumento: JSON.parse(localStorage.getItem('result')).ndoc
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.codigo === "00") {
+            localStorage.removeItem('result');
+            window.location.href = '/login.html';
+        } else {
+            alert("Error al cerrar sesión");
+        }
+    })
+    .catch(error => {
+        console.error('Error al cerrar sesión:', error);
+    });
 }
